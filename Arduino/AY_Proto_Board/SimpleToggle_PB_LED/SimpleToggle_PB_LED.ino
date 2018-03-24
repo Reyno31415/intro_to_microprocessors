636f9f434023be4e3bb3b5401e75e3f1f8ed18f3
Reyno31415 <33499904+Reyno31415@users.noreply.github.com>
2018-03-24 17:39:13 -0400

    Add files via upload

---
+++ Arduino/AY_Proto_Board/SimpleToggle_PB_LED/SimpleToggle_PB_LED.ino
@@ -0,0 +1,31 @@
+    /* SimpleToggle_PB_LED */
+    // Added code to turn on LED on startup
+
+#define LEDpin 3
+#define SWITCHpin 2
+
+byte LEDstate = HIGH;  //Set to turn on LED
+byte SWITCHstate;
+byte lastSWITCHstate;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+digitalWrite(LEDpin, LEDstate); //Turn on LED
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+SWITCHstate = digitalRead(SWITCHpin);
+if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+{
+    LEDstate = !LEDstate;
+    digitalWrite(LEDpin, LEDstate);
+    
+}
+lastSWITCHstate = SWITCHstate;
+}
