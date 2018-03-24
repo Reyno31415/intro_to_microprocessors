2f90a68b70a44da6d33cbd8d89f5bdf8efb6a1e4
Reyno31415 <33499904+Reyno31415@users.noreply.github.com>
2018-03-24 17:40:38 -0400

    Add files via upload

---
+++ Arduino/AY_Proto_Board/Simple_PB_LED_V2/Simple_PB_LED_V2.ino
@@ -0,0 +1,28 @@
+        /*Simple_PB_LED_V1        Greg Reynolds Nov 2016 */
+        
+// Define two constant names
+#define LEDpin 3
+#define SWITCHpin 2
+
+// Define a variable name and set LOW
+byte LEDstate = LOW;
+
+/* The following function is run once.
+   It sets up the I/O pins */
+void setup() {
+  //Define the pin driving the LED as an output//
+pinMode(LEDpin, OUTPUT);
+  //Define the pin connected to the switch as an input//
+pinMode(SWITCHpin, INPUT);
+  // Connect an internal pullup resistor to the switch pin//
+digitalWrite(SWITCHpin, HIGH);
+}
+
+/* The following function loops continuesly.
+   It is the "motor" of the design */
+void loop() {
+  // Read the status of the switch
+LEDstate = digitalRead(SWITCHpin);
+  // Write the status of the switch to the LED
+digitalWrite(LEDpin, LEDstate);
+}
